de4c45b9f15865c61be15169ee483c87028d77e6
Rider7723 <riderh911@gmail.com>
2025-05-16 14:11:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function sum(){
+const num1 = document.getElementById("num1").value;
+const num2 = document.getElementById("num2").value;
+document.getElementById("result").value = num1+num2;
+}
+
+function sub(){
+const num1 = document.getElementById("num1").value;
+const num2 = document.getElementById("num2").value;
+document.getElementById("result").value = num1-num2;
+}
+
+function mul(){
+const num1 = document.getElementById("num1").value;
+const num2 = document.getElementById("num2").value;
+document.getElementById("result").value = num1*num2;
+}
+
+function div(){
+const num1 = document.getElementById("num1").value;
+const num2 = document.getElementById("num2").value;
+document.getElementById("result").value = num1/num2;
+}
+
+function cle(){
+document.getElementById("result").value = null;
+}
+
